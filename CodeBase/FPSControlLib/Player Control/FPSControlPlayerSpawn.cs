7154b15d3a5d2b82d0f439dcf5f3ed4ce12b1903
Kreidian <eitan.s.levy@gmail.com>
2013-12-06 15:40:17 -0800

    re commiting missing player spawn files

---
+++ CodeBase/FPSControlLib/Player Control/FPSControlPlayerSpawn.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPSControl
+{    
+    public class FPSControlPlayerSpawn : MonoBehaviour
+    {
+        public int spawnID = 0;
+        public bool available = false;
+
+        private static FPSControlPlayerSpawn[] _spawnPoints = null;
+
+        void Start()
+        {
+            if (_spawnPoints == null)
+            {
+                _spawnPoints = FindObjectsOfType(typeof(FPSControlPlayerSpawn)) as FPSControlPlayerSpawn[];
+            }
+        }
+
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireSphere(transform.position, 0.3F);
+            Vector3 forward = transform.rotation * new Vector3(0, 0, 1);
+            Vector3 pos = transform.position;
+
+            Gizmos.DrawRay(pos, forward);
+
+            Vector3 right = Quaternion.Euler(0, 200, 0) * forward;
+            Vector3 left = Quaternion.Euler(0, 160, 0) * forward;
+            Gizmos.DrawRay(pos + forward, right * 2);
+            Gizmos.DrawRay(pos + forward, left * 2);
+
+        }
+
+        public static FPSControlPlayerSpawn[] GetAllSpawnPoints() { return _spawnPoints; }
+
+        public static void ResetSpawnPoints()
+        {
+            _spawnPoints = FindObjectsOfType(typeof(FPSControlPlayerSpawn)) as FPSControlPlayerSpawn[];
+        }
+
+        public static bool ReSpawn(FPSControlPlayer player)
+        {
+
+            FPSControlPlayerSpawn spawn = null;
+            float minDist = -1f;
+            float d = 0f;
+
+            foreach (FPSControlPlayerSpawn sp in _spawnPoints)
+            {
+                if (sp.available)
+                {
+                    d = Vector3.Distance(player.transform.position, sp.transform.position);
+
+                    if (minDist < 0 || d < minDist)
+                    {
+                        minDist = d;
+                        spawn = sp;
+                    }
+                }
+            }
+
+            if (spawn != null)
+            {
+                //Debug.Log(spawn);
+                Vector3 pos = spawn.transform.position;
+                player.transform.position = pos;
+                Quaternion rot = spawn.transform.rotation;
+                player.transform.rotation = rot;
+                player.playerCamera.ResetRotation();
+                
+                FPSControlPlayerEvents.Spawn();
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
